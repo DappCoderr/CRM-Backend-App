71164ef4d47b0eedbdb80932c4732ae1ece78b84
DappCoderr <68939930+DappCoderr@users.noreply.github.com>
2025-12-01 10:38:31 +0530

    setup redisClient

---
+++ utils/redisClient.js
@@ -0,0 +1,28 @@
+import Redis from "ioredis";
+
+// This file connects your NodeJS app to Redis.
+
+const createRedis = () => {
+
+    const REDIS_URL = process.env.REDIS_URL || 'redis://localhost:6379'
+    const client = new Redis(REDIS_URL, {
+        maxRetryPerRequest: null,
+        enableReadyCheck: true,
+    })
+
+    client.on("error", (error) => {
+        console.log("Redis error: ", error.message)
+    })
+
+    client.on("connect", ()=>{
+        console.log("[redis] connected")
+    })
+
+    client.on("ready", ()=>{
+        console.log("[redis] connnected")
+    })
+
+    return client
+}
+
+export default createRedis
